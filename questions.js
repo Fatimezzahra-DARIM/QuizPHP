85b9c8ed835ae67fb5a0640a9646ef5856c362a5
Fatimezzahra <fatimezzahradarim@gmail.com>
2022-12-21 17:30:20 +0100

    Creation of file JS

---
+++ questions.js
@@ -0,0 +1,43 @@
+//Creating an array and passing the number,questions,options, and 
+let questions = [
+  {
+    numb: 1,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Private Home Page",
+      "Private Hypertext Processor",
+      "Hypertext Preprocessor",
+      "Personal Hypertext Processor",
+    ],
+  },
+  {
+    numb: 2,
+    question: "In Object Oriented PHP, how to access attributes of a class?",
+    answer: "$this",
+    options: ["classe()", "return()", "$this", "attrib()"],
+  },
+  {
+    numb: 3,
+    question: "How to sort an array?",
+    answer: "sort()",
+    options: ["ascending()", "asc()", "sort()", "orderby()"],
+  },
+  {
+    numb: 4,
+    question: "The escape character is",
+    answer: "\\",
+    options: ['"', "\\", "/", "#"],
+  },
+  {
+    numb: 5,
+    question: "How to get the unique ID of a session?",
+    answer: "session_id()",
+    options: [
+      "$_SESSION['ID_PHP']",
+      "get_session()",
+      "session_id()",
+      "$_SESSION['ID']",
+    ],
+  },
+];
---
+++ script.js
@@ -0,0 +1,46 @@
+// getting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+// If start Quiz Button Clicked
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo"); //show the info box 
+}
+// If Exit Quiz Button Clicked
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo"); //Hide the info box
+};
+// If Continue Button Clicked
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo"); //Hide the info box
+    quiz_box.classList.add("activeQuiz"); //Show the quiz box
+    showQuestions(0);
+};
+let que_count = 0;
+const next_btn = quiz_box.querySelector(".next_btn");
+next_btn.onclick = () => {
+    if (que_count > questions.length - 1) {
+     que_count++;
+     showQuestions(que_count);   
+    } else {
+        
+    }
+    
+}
+//If Next Button Clicked
+//getting questions & options from array
+function showQuestions(index) {
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>' + questions[index].question + '</span>';
+    let option_tag =
+      '<div class="option">' +questions[index].options[0] + '<span></span> </div>'
+        +
+      '<div class="option">' +questions[index].options[1] + '<span></span> </div>' +
+      '<div class="option">' +questions[index].options[2] + '<span></span> </div>' +
+      '<div class="option">' +questions[index].options[3] + '<span></span> </div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+}   
